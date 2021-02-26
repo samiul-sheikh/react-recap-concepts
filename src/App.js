4f8bfbb8cfd522c3ecf8cfdc4f84fe040f640d61
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-02-26 22:53:39 +0600

    Pass data through Props in React

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+    const employees = ['Samiul', 'Chaity', 'Titul']
     return (
         <div className="App">
             {/* using first component */}
-            <Employee></Employee>
-            <Employee></Employee>
-            <Employee></Employee>
-            <Employee></Employee>
+            <Employee name={employees[0]} role="admin"></Employee>
+            <Employee name={employees[1]} role="general member"></Employee>
+            <Employee name="Forhad"></Employee>
+            <Employee name="Rakhee"></Employee>
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
             </header>
@@ -17,16 +18,21 @@ function App() {
 }
 
 // create first component
-function Employee() {
+function Employee(props) {
+    // show data details
+    console.log(props)
     const employeeStyle = {
-        border:'2px solid purple',
+        border: '2px solid purple',
+        borderRadius: '10px',
         margin: '20px',
         width: '500px'
     }
-    return <div style={employeeStyle}>
-        <h1>User Details</h1>
-        <h3>General Member</h3>
-    </div>
+    return (
+        <div style={employeeStyle}>
+            <h1>User-{props.name}</h1>
+            <h3>user position {props.role || "website viewer"}</h3>
+        </div>
+    )
 }
 
 export default App;
