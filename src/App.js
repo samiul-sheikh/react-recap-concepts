b47058071f3b0c7aad2b21ea88a64d901a39ea8f
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-02-26 22:23:12 +0600

    adding employee component & design

--- src/App.js
+++ src/App.js
@@ -2,13 +2,31 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+    return (
+        <div className="App">
+            {/* using first component */}
+            <Employee></Employee>
+            <Employee></Employee>
+            <Employee></Employee>
+            <Employee></Employee>
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+            </header>
+        </div>
+    );
+}
+
+// create first component
+function Employee() {
+    const employeeStyle = {
+        border:'2px solid purple',
+        margin: '20px',
+        width: '500px'
+    }
+    return <div style={employeeStyle}>
+        <h1>User Details</h1>
+        <h3>General Member</h3>
     </div>
-  );
 }
 
 export default App;
